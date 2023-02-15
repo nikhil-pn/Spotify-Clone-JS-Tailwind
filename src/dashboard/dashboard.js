f74ed9a31e3cd50d00929c9ea52a6eb537de096f
nikhilpn725 <nikhilpn725@gmail.com>
2023-02-15 17:49:06 +0530

    Logout btn added when clicked it logout!

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -2,23 +2,42 @@ import { fetchRequest } from "../api";
 import { ENDPOINT, logout } from "../common";
 const displayName = document.querySelector("#display-name");
 
+const onProfileClick = (event) => {};
+
+const defaultName = document.querySelector("#display-name");
+const defaultImage = document.querySelector("#default-image");
+const defaultButton = document.querySelector("#user-profile-button");
+
+const defaultLogOutButton = document.querySelector("#default-logout-button");
+
 const loadUserProfile = async () => {
-  const displayName = document.querySelector("#display-name");
-  const defaultImage = document.querySelector("#default-image");
-  const defaultButton = document.querySelector("#user-profile-button");
+  const { display_name: displayName, images } = await fetchRequest(
+    ENDPOINT.userInfo
+  );
 
-  const userInfo = await fetchRequest(ENDPOINT.userInfo);
+  defaultName.textContent = displayName;
 
-  return userInfo;
+  if (images?.length) {
+    defaultImage.classList.add("hidden");
+  } else {
+    defaultImage.classList.remove("hidden");
+  }
+
+  defaultButton.addEventListener("click", onProfileClick);
 };
 
 document.addEventListener("DOMContentLoaded", async () => {
-  displayName.addEventListener("click", () => {
-    console.log("clicked");
+  defaultButton.addEventListener("click", () => {
+    if (defaultLogOutButton.classList.contains("hidden")) {
+      defaultLogOutButton.classList.remove("hidden");
+    } else {
+      defaultLogOutButton.classList.add("hidden");
+    }
+  });
+
+  defaultLogOutButton.addEventListener("click", () => {
     logout();
   });
 
-  const userInfo = await  loadUserProfile();
-  console.log(userInfo);
-  displayName.textContent = userInfo.display_name;
+  loadUserProfile();
 });
