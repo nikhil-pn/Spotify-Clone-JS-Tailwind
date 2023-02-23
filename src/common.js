5023ed941afa14de84c2b82d5efbb82695a44d29
nikhilpn725 <nikhilpn725@gmail.com>
2023-02-23 15:15:58 +0530

    sidenav spotify logo and home btns created ui html

--- src/common.js
+++ src/common.js
@@ -9,6 +9,7 @@ export const ENDPOINT = {
   featuredPlayList: "browse/featured-playlists?limit=5",
   toplists: "browse/categories/toplists/playlists?limit=10",
   playlist: "playlists",
+  userPlaylist : "me/playlists"
 };
 
 //logout
